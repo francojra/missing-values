97c651ab7900fa67f972eb15c9d6d263043f2706
Jeanne Franco <44760301+francojra@users.noreply.github.com>
2023-12-02 23:47:46 -0300

    Add files via upload

--- missing_values.R
+++ missing_values.R
@@ -5,9 +5,19 @@
 
 # Introdução ---------------------------------------------------------------------------------------------------------------
 
-## Você já tem aprendido o básico sobre valores perdidos anteriormente neste livro.
+## Você já tem aprendido o básico sobre valores perdidos (missing values) 
+## anteriormente neste livro.
 ## Primeiro você viu sobre no capítulo 1 em que eles resultam em um aviso quando
 ## ao fazer um gráfico, bem como na seção 3.5.2 onde interferiram nos cálculos das
 ## estatísticas resumidas, e você aprendeu sobre sua natureza infecciosa e como
 ## verificar verificar sua presença na seção 12.2.2. Agora nós vamos voltar ao
 ## tema em mais profundidade, para que você aprenda mais detalhes.
+
+## Nós começaremos discutindo algumas ferramentas gerais para trabalhar com valores
+## perdidos gravados como NAs. Nós então exploraremos a ideia de valores implicitamente
+## ausentes, valores que são simplesmente ausentes nos seus dados, e mostrar algumas
+## ferramentas que você pode usar para tornar esses valores explícitos. Terminaremos 
+## com uma discussão relacionada sobre grupos vazios, causados por níveis de fatores 
+## que não aparecem nos dados.
+
+
