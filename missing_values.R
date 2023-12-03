0e3ecab5b402ec5ad31ee5c126c27edee7f59af6
Jeanne Franco <44760301+francojra@users.noreply.github.com>
2023-12-02 23:41:03 -0300

    Add files via upload

--- missing_values.R
+++ missing_values.R
@@ -4,3 +4,10 @@
 # Fonte: https://r4ds.hadley.nz/missing-values -----------------------------------------------------------------------------
 
 # Introdução ---------------------------------------------------------------------------------------------------------------
+
+## Você já tem aprendido o básico sobre valores perdidos anteriormente neste livro.
+## Primeiro você viu sobre no capítulo 1 em que eles resultam em um aviso quando
+## ao fazer um gráfico, bem como na seção 3.5.2 onde interferiram nos cálculos das
+## estatísticas resumidas, e você aprendeu sobre sua natureza infecciosa e como
+## verificar verificar sua presença na seção 12.2.2. Agora nós vamos voltar ao
+## tema em mais profundidade, para que você aprenda mais detalhes.
